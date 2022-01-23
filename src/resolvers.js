32d67c301a94b06afa12d4c07205003ce9752069
gabriel <gabyk800@gmail.com>
2022-01-23 16:27:57 +0200

    added prisma

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,33 +1,37 @@
+const { prisma } = require('./db');
+
 const resolvers = {
   Query: {
     feed: (parent, args) => {
-      return posts.filter((post) => post.published);
+      return prisma.post.findMany({
+        where: { published: true },
+      });
     },
     post: (parent, args) => {
-      return posts.find((post) => post.id === Number(args.id));
+      return prisma.post.findOne({
+        where: { id: Number(args.id) },
+      });
     },
   },
   Mutation: {
     createDraft: (parent, args) => {
-      posts.push({
-        id: posts.length + 1,
-        title: args.title,
-        content: args.content,
-        published: false,
+      return prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+        },
       });
-      return posts[posts.length - 1];
     },
     publish: (parent, args) => {
-      const postToPublish = posts.find((post) => post.id === Number(args.id));
-      postToPublish.published = true;
-      return postToPublish;
+      return prisma.post.update({
+        where: {
+          id: Number(args.id),
+        },
+        data: {
+          published: true,
+        },
+      });
     },
-  },
-  Post: {
-    content: (parent) => parent.content,
-    id: (parent) => parent.id,
-    published: (parent) => parent.published,
-    title: (parent) => parent.title,
   },
 };
 
